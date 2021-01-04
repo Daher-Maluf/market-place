9c4a1c3d0ba2b7a646fa4d574a30891cf374379e
fausto <faustomalufff@gmail.com>
2021-01-04 15:48:36 -0300

    se crearon los modulos necesarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './modules/header/header.component';
+import { HeaderPromotionComponent } from './modules/header-promotion/header-promotion.component';
+import { HeaderMobileComponent } from './modules/header-mobile/header-mobile.component';
+import { NewsLetterComponent } from './modules/news-letter/news-letter.component';
+import { FooterComponent } from './modules/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeaderPromotionComponent,
+    HeaderMobileComponent,
+    NewsLetterComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
